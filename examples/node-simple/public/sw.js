43d44663d006a797dc8e3acaff87eac9c48dddc4
Joost Faber <j.faber@hva.nl>
2021-03-16 09:32:53 +0100

    added cache-all-request features in comments

--- examples/node-simple/public/sw.js
+++ examples/node-simple/public/sw.js
@@ -2,36 +2,56 @@ const CORE_CACHE = 1
 const CORE_CACHE_NAME = `core-v${CORE_CACHE}`
 const CORE_ASSETS = ["manifest.json","/offline"] 
 
-self.addEventListener('install', (e) => {
+self.addEventListener('install', (event) => {
     console.log("Installed")
-    e.waitUntil(
+    event.waitUntil(
         caches.open(CORE_CACHE_NAME)
         .then(cache => cache.addAll(CORE_ASSETS))
         .then(() => self.skipWaiting())
     )
 })
 
-self.addEventListener("activate", (e) => {
+self.addEventListener("activate", (event) => {
     console.log("Activated")
-    e.waitUntil(clients.claim())
+    event.waitUntil(clients.claim())
 })
 
-self.addEventListener("fetch", (e) => {
-    
-    const req = e.request
+self.addEventListener("fetch", (event) => {
+    const req = event.request
     console.log("Fetching:" + req.url)
     
-    e.respondWith(
+    // show cached request from cache
+    event.respondWith(
         caches.match(req)
-        .then(cachedRes => {
-            if (cachedRes) {
-                return cachedRes
-            }
-            return fetch(req)
-                .then((fetchRes) => fetchRes)
-                .catch((err) => {
-                    return caches.open(CORE_CACHE_NAME)
-                    .then(cache => cache.match('/offline'))})
+            .then(cachedRes => {
+                if (cachedRes) {
+                    return cachedRes
+                }
+                return fetch(req)
+                    .then((fetchRes) => fetchRes)
+                    .catch((err) => {
+                        return caches.open(CORE_CACHE_NAME)
+                        .then(cache => cache.match('/offline'))})
         })
     )
+
+    /* Save all requests to cache */
+    // event.respondWith(
+    //     caches.open(CORE_CACHE_NAME).then(cache => {
+    //         return cache.match(event.request)
+    //             .then(response => {
+    //                 if(response) {
+    //                     return response
+    //                 }
+    //                 return fetch(event.request)
+    //                 .then(response => {
+    //                     cache.put(event.request, response.clone())
+    //                     return response
+    //                 })
+    //             }).catch((err) => {
+    //                 return caches.open(CORE_CACHE_NAME).then(cache => cache.match('/offline'))
+    //             })
+    //     })
+    // )
+            
 })
